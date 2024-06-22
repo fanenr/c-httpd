2723118e551e8529602c9d5b00a9db5399734800
Arthur63 <Arthur63@163.com>
2024-06-22 16:28:13 +0800

    tune some details

--- httpd.h
+++ httpd.h
@@ -7,7 +7,7 @@
 #include <netinet/in.h>
 #include <sys/socket.h>
 
-#define HTTPD_SERVER_REUSEADDR 1
+#define SERVER_REUSEADDR 1
 
 enum
 {
@@ -61,7 +61,6 @@ typedef struct sockaddr_in sockaddr4_t;
 typedef struct sockaddr_in6 sockaddr6_t;
 
 typedef struct server_t server_t;
-typedef struct client_t client_t;
 
 struct server_t
 {
@@ -70,13 +69,6 @@ struct server_t
   uint16_t port;
   sockaddr4_t addr;
   threadpool_t pool;
-};
-
-struct client_t
-{
-  int sock;
-  server_t *serv;
-  sockaddr4_t addr;
 };
 
 extern void server_free (server_t *serv);
